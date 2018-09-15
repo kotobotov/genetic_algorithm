98f586c9ea960893ce8b3413a584f44f9d581a2f
Kotobotov.ru <dima@kotobotov.ru>
2018-09-15 12:48:34 +0700

    :white_check_mark: Adding tests.
    Took 27 seconds

--- src/test/scala/GeneticTest.scala
+++ src/test/scala/GeneticTest.scala
@@ -1,4 +1,5 @@
 import org.scalatest.FreeSpec
+//import GeneticAlgorithm._
 
 /**
   * Created by Kotobotov.ru on 13.09.2018.
@@ -7,13 +8,20 @@ class GeneticTest extends FreeSpec {
 
   "Correctnes" - {
     "should have solution" in {
-      assert(Set.empty.size == 0)
+      val geneBase = "papapa"
+      val geneSize = 10
+     val target = "How many monkeys does it take to produce Shakespeare?"
+      def fitnes(DNA: DNA) = 0.0
+      val mutationRate = 3
+      assert(
+        new GeneticAlgorithm(geneBase, geneSize, fitnes, mutationRate)
+          .runEvalution()
+          .maxScore == 1)
     }
 
     "should have impruving" in {
       assert(Set.empty.size == 0)
     }
-
 
     "don't have solution" in {
       assertThrows[NoSuchElementException] {
